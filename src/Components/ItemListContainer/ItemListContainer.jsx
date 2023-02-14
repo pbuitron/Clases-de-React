bf623c05c031fb6d8273b39186791d93ba80a19e
pbuitron <pbuitron@users.noreply.github.com>
2023-02-13 22:59:55 -0500

    Segunda Preentrega - Paul Buitron

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -26,7 +26,7 @@ export const ItemListContainer = () => {
     }, [idCategoria]);
 
     return (
-        <div className="row row-cols-1 row-cols-md-4 g-5">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 ">
             {productos}
         </div>
     );
